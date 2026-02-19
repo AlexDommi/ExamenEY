42ae6768bec09b27f68ee53a268459a853e0b3c7
AlexSD <jesus2209.isc@gmail.com>
2026-02-19 14:23:51 -0600

    Se agregan Cambios y tambien DashBoard
    Se agregan Cambios y tambien DashBoard

---
+++ ExamenJASD/wwwroot/js/Dashboards.js
@@ -0,0 +1,14 @@
+﻿
+const ctx = document.getElementById('graficaUsuarios');
+
+new Chart(ctx, {
+    type: 'bar', // puedes cambiar a 'line', 'pie', etc.
+data: {
+    labels: ['Total Usuarios', 'Usuarios Activos', 'Usuarios Inactivos'],
+datasets: [{
+    label: 'Usuarios',
+    data: [totalUsuarios, usuariosActivos, usuariosInactivos],
+borderWidth: 1
+        }]
+    }
+});

4c2acb0468dec2e9de5763074449bfc6fc35c7e8
AlexSD <jesus2209.isc@gmail.com>
2026-02-19 14:56:27 -0600

    x
    x

--- ExamenJASD/obj/Debug/net10.0/ExamenJASD.AssemblyInfo.cs
+++ ExamenJASD/obj/Debug/net10.0/ExamenJASD.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExamenJASD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5205c3e05d60d7917dfc815f791282c6b44329a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cdde8702ee0ed78e26c4c6d6fd530407fb6d8a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExamenJASD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExamenJASD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
